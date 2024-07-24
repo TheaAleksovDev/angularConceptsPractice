4848593d9315b95c16f59255652b97aedd136f41
TheaAleksovDev <aleksov.tea@gmail.com>
2024-07-24 20:31:06 +0200

    practice observable and subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PracticeComponent } from './components/practice/practice.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PracticeComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'rxjs-autocomplete-search';
+
+  selectedComponent!: number;
+
+  setSelectedComponent(id: number) {
+    this.selectedComponent = id;
+  }
 }
